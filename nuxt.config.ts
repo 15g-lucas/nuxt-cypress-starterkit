60ccbd3f957236d88c9f3e4324631f5760f7a699
Lucas <gosgnach24lucas@gmail.com>
2025-02-10 09:03:10 +0100

    ⚙️ Add nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  server: {
-    port: 3001,
-    host: '0.0.0.0',
-  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
 })
